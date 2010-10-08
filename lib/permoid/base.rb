479b098eb5b81e9eef7d7d8b5387422545bf92b6
Cyril Mougel <cyril.mougel@gmail.com>
2010-10-08 11:30:26 +0200

    add field_permalink method to define the field using to be a permalink

--- lib/permoid/base.rb
+++ lib/permoid/base.rb
@@ -1,18 +1,18 @@
 module Permoid
-  
+
   module Base
     def self.included(klass)
       klass.class_eval do
         raise if !self.include? Mongoid::Document
-        
-        
+
+
         field                    :permalink
         before_validation        :set_permalink
-        
+
         def set_permalink(permalink = nil)
-          return if self.title.nil? 
-          permalink = permalink.nil? ? self.title.to_permalink : permalink
-          
+          return if self.field_permalink.nil?
+          permalink = permalink.nil? ? self.field_permalink.to_permalink : permalink
+
           others = self.class.where(:permalink => permalink)
           others = others.excludes(:id => self.id) unless self.new_record?
           index = 0
@@ -20,15 +20,23 @@ module Permoid
             index += 1
             others = self.class.where(:permalink => "#{permalink}-#{index}")
           end
-          
+
           self.permalink = index > 0 ? "#{permalink}-#{index}" : permalink
         end
-        
+
         def to_param
           self.permalink
         end
+
+        def field_permalink
+          send(@@field_permalink)
+        end
+
+        def self.field_permalink(field)
+          @@field_permalink = field
+        end
       end
     end
-    
+
   end
 end
